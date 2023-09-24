905b25608c03110dc541a107834bb87a4881f63c
kenneditodd <kenneditodd@gmail.com>
2023-09-24 17:19:05 -0500

    add .snakemake folder

--- bulkRNA/scripts/01_preprocessing/04_create_config.py
+++ bulkRNA/scripts/01_preprocessing/04_create_config.py
@@ -8,31 +8,26 @@ allSamples = list()
 read = ["R1", "R2"]
 numSamples = 0
 
-with open('../../refs/fastq_headers.tsv', 'r') as infile:
+with open('../../refs/sample_file_list.txt', 'r') as infile:
     for line in infile:
         numSamples += 1
-        split = line.split()
-        fileName = split[0]  # Mayo_43_P_S17_L001_R1_001.fastq.gz, for each sample there are 2 reads in 2 lanes (4 files)
-        allSamples.append(fileName.replace("_L001_R1_001.fastq.gz", ""))
-        if line.__contains__("_M_"):
-            meningesSamples.append(fileName.replace("_L001_R1_001.fastq.gz", ""))
-        if line.__contains__("_P_"):
-            parietalSamples.append(fileName.replace("_L001_R1_001.fastq.gz", ""))
+        sample = line.strip()
+        allSamples.append(sample.replace("_L001_R1_001.fastq.gz", ""))
 
 # create header and write to outfile
 header = '''{{
     "DIRECTORIES",
-    "rawReads" : "/research/labs/neurology/fryer/projects/aducanumab/mouse/bulkRNA",
+    "rawReads" : "/research/labs/neurology/fryer/projects/aducanumab/mouse/bulkRNA/",
     "rawQC" : "rawQC/",
     "trimmedReads" : "trimmedReads/",
     "trimmedQC" : "trimmedQC/",
     "starAligned" : "starAligned/",
     "featureCounts" : "featureCounts/",
-    "genomeDir" : "/research/labs/neurology/fryer/projects/references/mouse/refdata-gex-mm10-2020-A/star/",
+    "genomeDir" : "refs/starGenomeDir/",
 
     "FILES",
-    "Mmusculus.gtf" : "/research/labs/neurology/fryer/projects/references/mouse/genes/genes.gtf",
-    "Mmusculus.fa" : "/research/labs/neurology/fryer/projects/references/mouse/fasta/genome.fa",
+    "Mmusculus.gtf" : "/research/labs/neurology/fryer/projects/references/mouse/refdata-gex-mm10-2020-A/genes/genes.gtf",
+    "Mmusculus.fa" : "/research/labs/neurology/fryer/projects/references/mouse/refdata-gex-mm10-2020-A/fasta/genome.fa",
 
     "SAMPLE INFORMATION",
     "allSamples": {0},
@@ -43,41 +38,41 @@ header = '''{{
 '''
 outfile.write(header.format(allSamples, read))
 
+
 # config formatting
 counter = 0
-with open('../../refs/fastq_headers.tsv', 'r') as infile:
+with open('../../refs/sample_file_list.txt', 'r') as infile:
     for line in infile:
         counter += 1
 
-        # store sample name and info, Mayo_43_P_S17_L001_R1_001.fastq.gz, for each sample there is 2 reads in 2 lanes (4 files)
-        split = line.split()
-        lane1read1 = split[0].replace(".fastq.gz", "")
-        lane1read2 = lane1read1.replace("R1", "R2")
-        lane2read1 = lane1read1.replace("L001","L002")
-        lane2read2 = lane1read2.replace("L001","L002")
-        baseName = lane1read1.replace("_L001_R1_001", "")
-        sampleInfo = split[1]
+        # store filename
+        sample = line.strip()
+        lane1read1 = sample.replace("L001_R1_001.fastq.gz", "L001_R1")
+        lane1read2 = sample.replace("L001_R1_001.fastq.gz", "L001_R2")
+        lane2read1 = sample.replace("L001_R1_001.fastq.gz", "L002_R1")
+        lane2read2 = sample.replace("L001_R1_001.fastq.gz", "L002_R2")
+        lane3read1 = sample.replace("L001_R1_001.fastq.gz", "L003_R1")
+        lane3read2 = sample.replace("L001_R1_001.fastq.gz", "L003_R2")
+        lane4read1 = sample.replace("L001_R1_001.fastq.gz", "L004_R1")
+        lane4read2 = sample.replace("L001_R1_001.fastq.gz", "L004_R2")
+        baseName = sample.replace("_L001_R1_001.fastq.gz", "")
 
         # break down fastq file info
         # @A00127:312:HVNLJDSXY:2:1101:2211:1000
         # @<instrument>:<run number>:<flowcell ID>:<lane>:<tile>:<x-pos>:<y-pos>
-        sampleInfo = sampleInfo.split(':')
-        instrument = sampleInfo[0]
-        instrument = instrument.replace("@", "")
-        runNumber = sampleInfo[1]
-        flowCell = sampleInfo[2]
 
         out = '''
     "{0}":{{
         "lane1read1": "{1}",
-        "lane2read1": "{2}",
-        "lane1read2": "{3}",
+        "lane1read2": "{2}",
+        "lane2read1": "{3}",
         "lane2read2": "{4}",
-        "instrument": "{5}",
-        "runNumber": "{6}",
-        "flowCell": "{7}"
+        "lane3read1": "{5}",
+        "lane3read2": "{6}",
+        "lane4read1": "{7}",
+        "lane4read2": "{8}"
         '''
-        outfile.write(out.format(baseName, lane1read1, lane2read1, lane1read2, lane2read2, instrument, runNumber, flowCell))
+        outfile.write(out.format(baseName, lane1read1, lane1read2, lane2read1, lane2read2, lane3read1, lane3read2, lane4read1, lane4read2))
         if (counter == numSamples):
             outfile.write("}\n}")
         else:
--- bulkRNA/scripts/01_preprocessing/05_run_snakemake.sh
+++ bulkRNA/scripts/01_preprocessing/05_run_snakemake.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem 2G
 #SBATCH --mail-user todd.kennedi@mayo.edu
 #SBATCH --mail-type END,FAIL
-#SBATCH --output logs/%x.%j.stdout
+#SBATCH --output logs/%x.%N.%j.stdout
 #SBATCH --error logs/%x.%j.stderr
 #SBATCH --partition cpu-short
-#SBATCH --time 08:00:00 ## HH:MM:SS
+#SBATCH --time 24:00:00 ## HH:MM:SS
 
 # activate conda environment
 source $HOME/.bash_profile
@@ -18,4 +18,4 @@ pwd
 
 # run snakemake
 # dry run: snakemake -nr
-snakemake -s Snakefile -j 20 --rerun-incomplete --latency-wait 20 --cluster "sbatch --mem=40G --output=scripts/01_preprocessing/logs/snakemake_job_logs/%x.%N.%j.stdout --error=scripts/snakemake/logs/%x.%N.%j.stderr --partition=cpu-short --tasks=20 --time 05:00:00"
+snakemake -s Snakefile -j 20 --rerun-incomplete --latency-wait 60 --cluster "sbatch --mem=40G --output=scripts/01_preprocessing/logs/snakemake_job_logs/%x.%N.%j.stdout --error=scripts/01_preprocessing/logs/snakemake_job_logs/%x.%N.%j.stderr --partition=cpu-short --tasks=20 --time 05:00:00"
