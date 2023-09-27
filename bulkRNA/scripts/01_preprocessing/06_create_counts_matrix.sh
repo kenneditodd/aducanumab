57fdb5a0d309b878491dee5b9c6b90ac50d18735
kenneditodd <kenneditodd@gmail.com>
2023-09-27 00:04:06 -0500

    update

--- bulkRNA/scripts/01_preprocessing/06_create_counts_matrix.sh
+++ bulkRNA/scripts/01_preprocessing/06_create_counts_matrix.sh
@@ -7,7 +7,7 @@ cd ../../featureCounts
 # GENE COUNT MATRIX
 #-----------------------------------------------------------------------------------------
 
-# create list of meninges gene featureCount count files
+# create list of gene featureCount count files
 ls -1 | grep gene.counts$ > gene_count_files.txt
 
 # store number of files in variable
@@ -46,52 +46,8 @@ else
 fi
 
 # cleanup
-rm meninges_count_files.txt
+rm gene_count_files.txt
 
-
-# EXON COUNT MATRIX
-#-----------------------------------------------------------------------------------------
-
-# create list of meninges gene featureCount count files
-ls -1 | grep _M_ | grep sex_specific | grep gene.counts$ > meninges_count_files.txt
-
-# store number of files in variable
-n=$(cat meninges_count_files.txt | wc -l)
-
-# if there are at least two files
-if [ $n -ge 2 ]
-then 
-	# get Geneid and counts column of first file
-	firstFile=$(head -n 1 meninges_count_files.txt)
-	tail -n+2 $firstFile | cut -f1,7 > file1.txt
-	
-	# get counts column of remaining files
-	i=2
-	for file in $(tail -n+2 meninges_count_files.txt)
-	do
-		tail -n+2 $file | cut -f7 > file$i;
-		let "i+=1"
-	done
-
-	# paste files together
-	paste -d "\t" file* > meninges_sex_specific_counts_matrix.tsv					
-
-	# rename columns
-	sed -i 's,starAligned/,,g' meninges_sex_specific_counts_matrix.tsv
-	sed -i 's,.Aligned.sortedByCoord.out.bam,,g' meninges_sex_specific_counts_matrix.tsv
-	sed -i 's/Geneid/ID/g' meninges_sex_specific_counts_matrix.tsv
-
-	# cleanup
-	rm file*
-	
-	# print
-	echo "Sex specific meninges counts matrix successfully generated."
-else
-	echo "Could not generate sex specific meninges counts matrix. You need at least two samples to generate this tissue matrix."
-fi
-
-# cleanup
-rm meninges_count_files.txt
 
 # SUMMED EXONS
 #-----------------------------------------------------------------------------------------
--- bulkRNA/scripts/02_metadata/02_create_metadata.R
+++ bulkRNA/scripts/02_metadata/02_create_metadata.R
@@ -8,20 +8,18 @@ setwd(".")
 mika.meta <- read.table("../../refs/mika_metadata.tsv", sep = "\t", header = TRUE)
 colnames(mika.meta) <- c("mouse_id","week","treatment","sex","RIN")
 
-samples <- read.table("../../refs/sample_file_list.txt", sep = "\t", header = TRUE)
+samples <- read.table("../../refs/sample_file_list.txt", sep = "\t", header = FALSE)
 colnames(samples) <- "filename"
-samples$mouse_id <- str_match(samples$filename, "([0-9]+)_[AduIgG_R]+_S[0-9]")[,2]
+samples$mouse_id <- str_match(samples$filename, "([0-9]+)_[AduIgG_R]+_S[0-9]+")[,2]
 samples$mouse_id <- as.numeric(samples$mouse_id)
 
-df <- dplyr::left_join(mika.meta, samples, by = "mouse_id")
+df <- dplyr::left_join(samples, mika.meta, by = "mouse_id")
 remove(mika.meta, samples)
 
 df$sex <- gsub("Female", "F", df$sex)
 df$sex <- gsub("Male", "M", df$sex)
 
 df$sample <- paste(df$treatment, df$week, df$sex, df$mouse_id, sep = ".")
-
-df <- df[,c(7,3,2,4,1,5,6)]
 
 write.table(df, "../../refs/metadata.tsv", quote = FALSE, sep = "\t")
 
